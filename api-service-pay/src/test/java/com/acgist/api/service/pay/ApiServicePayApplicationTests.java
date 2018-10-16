2c8431b8f5daa6305d0c8ea03aefed73e525a4af
acgist <289547414@qq.com>
2018-10-16 17:43:03 +0800

    eureka server

--- api-service-pay/src/test/java/com/acgist/api/service/pay/ApiServicePayApplicationTests.java
+++ api-service-pay/src/test/java/com/acgist/api/service/pay/ApiServicePayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.acgist.api.common;
+package com.acgist.api.service.pay;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApiCommonApplicationTests {
+public class ApiServicePayApplicationTests {
 
 	@Test
 	public void contextLoads() {
