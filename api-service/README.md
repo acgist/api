fc2dd8bd3025c126517d798b5430a970af2a1456
acgist <289547414@qq.com>
2018-10-18 20:34:37 +0800

    静态资源

--- api-service/README.md
+++ api-service/README.md
@@ -1,5 +1,6 @@
 # 目录结构
 ├── api-service-admin：系统服务  
+├── api-service-www：网站服务  
 ├── api-service-asyn：异步服务  
 ├── api-service-order：订单服务  
-└── api-service-www：网站服务  
+└── api-service-resources：静态资源服务  
---
+++ api-service/api-service-resources/src/main/java/com/acgist/controller/APIControllerAdvice.java
@@ -0,0 +1,35 @@
+package com.acgist.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.acgist.api.APICode;
+import com.acgist.exception.ErrorCodeException;
+import com.acgist.utils.RedirectUtils;
+
+/**
+ * 异常处理
+ */
+@ControllerAdvice
+public class APIControllerAdvice {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(APIControllerAdvice.class);
+	
+	@ExceptionHandler(Exception.class)
+	public void exception(HttpServletRequest request, HttpServletResponse response, Exception e) {
+		LOGGER.error("系统异常", e);
+		if (e instanceof ErrorCodeException) {
+			ErrorCodeException exception = (ErrorCodeException) e;
+			APICode code = APICode.valueOfCode(exception.getErrorCode());
+			RedirectUtils.error(code, exception.getMessage(), request, response);
+		} else {
+			RedirectUtils.error(APICode.CODE_9999, request, response);
+		}
+	}
+
+}
