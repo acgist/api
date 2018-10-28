baecfb6de318e444333fcf45b5374e2192ee2774
acgist <289547414@qq.com>
2018-10-28 20:12:33 +0800

    代码优化

--- api-common/api-common-order/src/main/java/com/api/core/order/service/IOrderService.java
+++ api-common/api-common-order/src/main/java/com/api/core/order/service/IOrderService.java
@@ -1,7 +1,6 @@
 package com.api.core.order.service;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.api.core.config.APIConstURL;
@@ -19,6 +18,6 @@ public interface IOrderService {
 	 * 创建订单
 	 */
 	@PostMapping(APIConstOrderURL.URL_ORDER)
-	OrderMessage order(@RequestBody OrderEntity order);
+	OrderMessage order(OrderEntity order);
 
 }
