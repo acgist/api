720880a13533797bd9c7764900df341570cd7057
acgist <289547414@qq.com>
2018-11-16 17:32:58 +0800

    发送邮件使用线程池，防止单个邮件发送时间过长导致线程阻塞

--- api-service/api-service-asyn/src/main/java/com/api/core/asyn/stream/MailMesssageReceiver.java
+++ api-service/api-service-asyn/src/main/java/com/api/core/asyn/stream/MailMesssageReceiver.java
@@ -1,5 +1,8 @@
 package com.api.core.asyn.stream;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +15,8 @@ import com.api.core.asyn.pojo.message.MailMessage;
 import com.api.core.asyn.service.MailService;
 
 /**
- * 邮件信息队列接收器
+ * 邮件信息队列接收器<br>
+ * 使用线程池发送，防止单个邮件发送时间过长导致线程阻塞
  */
 @EnableBinding(MailReceiverBinding.class)
 public class MailMesssageReceiver {
@@ -22,13 +26,18 @@ public class MailMesssageReceiver {
 	@Autowired
 	private MailService mailService;
 	
+	// 发送线程池
+	private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(20);
+	
 	@StreamListener(MailReceiverBinding.MAIL_RECEIVER_STREAM_BINDER)
 	public void receive(Message<MailMessage> message) {
 		MailMessage mailMessage = message.getPayload();
 		if(mailMessage == null) {
 			LOGGER.warn("发送邮件信息为空");
 		} else {
-			mailService.post(mailMessage.getTo(), mailMessage.getSubject(), mailMessage.getContent());
+			EXECUTOR.submit(() -> {
+				mailService.post(mailMessage.getTo(), mailMessage.getSubject(), mailMessage.getContent());
+			});
 		}
 	}
 
